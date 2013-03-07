271fe7a435142655c6074b4fa2d06b9512332877
derekstory <derek.a.story@gmail.com>
2013-03-07 15:24:38 -0600

    new files layed out - removed all db connections until scripting begins - modified header menu

---
+++ faq.php
@@ -0,0 +1,25 @@
+<?php
+include 'header.php';
+?>
+
+<div id="termsandconditions">
+   <h1 align="left">Frequently Asked Questions</h1>
+
+      <h4 align="left" style="margin-top:50px">Why did you decide to make this site?</h4>
+      <p>Ex pro sint habemus. Has novum iisque urbanitas in, ne melius civibus duo. Populo deleniti ius an, et legere possim aliquam eos. Id ludus saperet posidonium vix, et eos lorem mazim. Est unum democritum ei, id quod affert vituperata sea, vel et odio invenire consequuntur. Vel ut iusto laboramus, no vim antiopam suscipiantur concludaturque, agam aperiri ex duo. Te case propriae qui, vis an placerat nominati, movet labore et nam.</p>
+
+ <h4 align="left">What is the ultimate goal of Braving Infinity?</h4>
+      </p>Audiam postulant cu est, brute illum legendos nam ei. At mei persius repudiare, quis saepe referrentur ad per. Scripta accusam ei usu. Eam et dicit saepe. Ius quod dicit viris eu, cu iudico philosophia cum, usu option epicuri at.</p>
+ <h4 align="left">How can I contact Braving Infinity to make a suggestion?</h4>
+      <p>Doctus percipit cotidieque id vix, te vel velit zril, omittam mandamus an qui. Ei cum aperiam repudiare, no labores voluptua his, vel audiam interesset dissentiet cu. Rebum legimus nam cu. Nam et adhuc commodo omittam, vim quis petentium expetendis no, amet similique ex eam. Nam cu recusabo constituam suscipiantur, minim populo ex vim.Lorem ipsum dolor sit amet, eam ad virtute propriae eloquentiam, no decore soleat legimus sit. Hinc feugait volutpat sed an, pri nobis iracundia ad. Ad sit cibo paulo, ius no paulo eleifend, modo oblique singulis pro cu. Prodesset omittantur ex pri, vix at iudico reprimique.Verear iisque similique quo at, vel te nisl delenit habemus. Suas purto mea an, vim ad nominavi appareat voluptaria. Possit vocent convenire pri ne. Duis brute assueverit has ex.</p>
+
+ <h4 align="left">A function with the site doesn't seem to work for me. How can I get it working?</h4>
+      <p>Ex pro sint habemus. Has novum iisque urbanitas in, ne melius civibus duo. Populo deleniti ius an, et legere possim aliquam eos. Id ludus saperet posidonium vix, et eos lorem mazim. Est unum democritum ei, id quod affert vituperata sea, vel et odio invenire consequuntur. Vel ut iusto laboramus, no vim antiopam suscipiantur concludaturque, agam aperiri ex duo. Te case propriae qui, vis an placerat nominati, movet labore et nam.Audiam postulant cu est, brute illum legendos nam ei. At mei persius repudiare, quis saepe referrentur ad per. Scripta accusam ei usu. Eam et dicit saepe. Ius quod dicit viris eu, cu iudico philosophia cum, usu option epicuri at.</p>
+
+ <h4 align="left">How can I get involved?</h4>
+      <p>Doctus percipit cotidieque id vix, te vel velit zril, omittam mandamus an qui. Ei cum aperiam repudiare, no labores voluptua his, vel audiam interesset dissentiet cu. Rebum legimus nam cu. Nam et adhuc commodo omittam, vim quis petentium expetendis no, amet similique ex eam. Nam cu recusabo constituam suscipiantur, minim populo ex vim.</p>
+</div>
+
+<?php
+include 'footer.php';
+?>
---
+++ popular.php
@@ -0,0 +1,50 @@
+<?php
+include 'header.php';
+?>
+
+<h1 align="center" style="background:#000;color:#fff;margin-top:70px;margin-bottom:-92px;width:100%;height:50px;padding-top:20px">Today's Most Popular</h1>
+   <div id="content">
+        <table class="width-100 bordered" id="list">
+            <thead class="thead-black">
+                <tr class="breakloop">
+                    <th class= "text-centered">Goal</th>
+                    <th class= "text-centered">Author</th>
+                    <th class= "text-centered">Views</th>
+                    <th class= "text-centered">Thoughts</th>
+                    <th class= "text-centered">Encouragers</th>
+                    <th class= "text-centered">Date</th>
+                </tr>
+            </thread>
+
+<?php
+
+$sql = "SELECT * FROM users, posts LIMIT 0, 20 ";
+
+$result = mysql_query($sql);
+
+while($row = mysql_fetch_assoc($result))
+{
+echo           '<tbody class="breakloop">
+
+                <tr>
+                    <td class="centered">
+                        <a href="/vdoc/download/00000993">
+                        <a href="/vdoc/edit/00000993">00000993</a>
+                    </td>
+                    <td>' . $row['post_title'] . '</td>
+                    <td class="centered">Politics</td>
+                        <a href="/vdoc/category/edit/"></a>
+                    <td class="centered">' . $row['user_name'] . '</td>
+                    </td>
+                    <td class="centered">24</td>
+                    <td class="centered">8</td>
+                    <td class="centered">' . $row['post_date'] . '</td>
+               </tr>';
+}
+?>
+
+        </tbody>
+        </table>
+<?php
+include 'footer.php';
+?>
