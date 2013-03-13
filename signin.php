8cefeddc456fe859a4b83ff106bf68bc8ef746e2
derekstory <derek.a.story@gmail.com>
2013-03-13 16:50:57 -0500

    Created two different pages for signin and signup

---
+++ signin.php
@@ -0,0 +1,24 @@
+<?php
+include 'connect.php';
+include 'header.php';
+?>
+
+
+<div id="signin">
+<h1 style="color:#fff;margin-top:200px">-Sign-In-</h1>
+ <form>
+  <form action="#">
+           <p><label for="user_name">User Name</label> <input type="text" id="user_name" style="height:50px;text-align:left; width:100%" /></p>
+           <p><label for="user_pass">Password</label> <input type="PASSWORD" id="user_pass" style="height:50px;text-align:left; width: 100%"/></p>
+                    <p class="submit"><input type="submit" value="Sign-In" style="height:25px;width:100px;background:#fff;color:#000;display:block" /></p>
+ </form>
+</div>
+?>
+
+</div>
+
+
+
+<?php
+include 'footer.php';
+?>
--- signup.php
+++ signup.php
@@ -3,16 +3,6 @@ include 'connect.php';
 include 'header.php';
 ?>
 
-<h1 style="color:#fff;margin-top:200px;margin-left:30px;margin-bottom:-80px">-Sign-In-</h1>
-<div id="signin">
- <form>
-  <form action="#">
-           <p><label for="user_name">User Name</label> <input type="text" id="user_name" style="height:50px;text-align:left; width:100%" /></p>
-           <p><label for="user_pass">Password</label> <input type="PASSWORD" id="user_pass" style="height:50px;text-align:left; width: 100%"/></p>
-                    <p class="submit"><input type="submit" value="Sign-In" style="height:25px;width:100px;background:#fff;color:#000;display:block" /></p>
- </form>
-</div>
-
 
 <div id="contentsignup">
 <h1 style="color:#fff">-Register-</h1>
@@ -95,7 +85,7 @@ else
                 }
                 else
                 {
-			echo 'Succesfully registered. You can now sign in and start posting!';
+			echo 'Succesfully registered. You can now <a href="signin.php">sign in</a> and start posting!';
                 }
          }
 
